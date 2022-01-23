55d9338163fa68cc471883ace93283a27022c949
George Mejia <gmejiadev@gmail.com>
2022-01-22 23:20:54 -0600

    first commit

--- src/components/InvoiceNavbar.vue
+++ src/components/InvoiceNavbar.vue
@@ -2,7 +2,7 @@
   <div class="max-w-screen-sm mx-auto mb-10 flex justify-between items-center">
     <div>
       <h1 class="text-white text-4xl font-bold mb-2">Invioces</h1>
-      <p class="text-white">There are 7 total invoices</p>
+      <p class="text-white">There are 5 total invoices</p>
     </div>
     <div class="flex items-center">
       <p class="text-white mr-5">Filter by status</p>
