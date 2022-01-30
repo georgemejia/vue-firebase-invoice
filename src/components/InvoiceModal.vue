df21689d9425ff6e3a88a95798061e67e2c2a1bd
George Mejia <gmejiadev@gmail.com>
2022-01-29 18:52:27 -0600

    split modal form into components

--- src/components/InvoiceModal.vue
+++ src/components/InvoiceModal.vue
@@ -1,88 +1,19 @@
 <template>
   <div class="w-full max-w-screen-md min-h-screen bg-slate-800 absolute top-0 left-0">
     <div class="container p-12 mx-auto">
-
-      <div class="text-indigo-600 text-sm font-bold mb-2">Bill From</div>
-      <label class="w-full block text-slate-300 text-sm mb-5" for="street">Street Address
-        <input 
-          class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="street" type="text" placeholder="Street Address"
-          v-model='invoice.billFromAddress'
-        >
-      </label>
-      
-      <div class="flex mb-10">
-        <label class="w-full block text-slate-300 text-sm" for="city">City
-          <input 
-            class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="city" type="text" placeholder="City"
-            v-model='invoice.billFromCity'
-          >
-        </label>
-        <label class="w-full block mx-3 text-slate-300 text-sm" for="code">Zip Code
-          <input 
-            class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="code" type="text" placeholder="Code"
-            v-model='invoice.billFromZipCode'
-          >
-        </label>
-        <label class="w-full block text-slate-300 text-sm" for="country">State
-          <input 
-            class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="country" type="text" placeholder="State"
-            v-model='invoice.billFromState'
-          >
-        </label>
-      </div>
-      <!-- Bill To -->
-      <div class="text-indigo-600 text-sm font-bold mb-2">Bill To</div>
-      <label class="w-full block text-slate-300 text-sm mb-5" for="street">Client Name
-        <input 
-          class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="street" type="text" placeholder="Name"
-          v-model='invoice.billToName'  
-        >
-      </label>
-      <label class="w-full block text-slate-300 text-sm mb-5" for="street">Client Email
-        <input 
-          class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="street" type="text" placeholder="Email"
-          v-model='invoice.billToEmail'
-        >
-      </label>
-      <label class="w-full block text-slate-300 text-sm mb-5" for="street">Street Address
-        <input 
-          class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="street" type="text" placeholder="Street Address"
-          v-model='invoice.billToAddress'  
-        >
-      </label>
-
-      <div class="flex mb-10">
-        <label class="w-full block text-slate-300 text-sm" for="city">City
-          <input 
-            class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="city" type="text" placeholder="City"
-            v-model='invoice.billToCity'
-          >
-        </label>
-        <label class="w-full block mx-3 text-slate-300 text-sm" for="code">Zip Code
-          <input 
-            class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="code" type="text" placeholder="Code"
-            v-model='invoice.billToZipCode'
-          >
-        </label>
-        <label class="w-full block text-slate-300 text-sm" for="country">State
-          <input 
-            class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="country" type="text" placeholder="State"
-            v-model='invoice.billToState'
-          >
-        </label>
-      </div>
-
+      <InvoiceBillFrom :from="invoice.from" />
+      <InvoiceBillTo :to="invoice.to" />
       <InvoiceModalItems :items="invoice.items" />
 
       <button 
-        class="text-white font-bold rounded bg-indigo-600 py-2 px-4"
+        class="text-white font-bold rounded bg-indigo-600 py-2 px-4" 
         @click="toggleModal">
         Cancel
       </button>
 
       <button 
         class="text-white font-bold rounded bg-indigo-600 py-2 px-4 ml-2"
-        @click="save">
+        @click="saveInvoice">
         Save
       </button>
 
@@ -93,29 +24,37 @@
 <script>
 import { reactive } from 'vue'
 import { useStore } from 'vuex'
+import InvoiceBillFrom from './InvoiceBillFrom.vue'
+import InvoiceBillTo from './InvoiceBillTo.vue'
 import InvoiceModalItems from './InvoiceModalItems.vue'
 export default {
   components: {
+    InvoiceBillFrom,
+    InvoiceBillTo,
     InvoiceModalItems
   },
   setup() {
     const store = useStore()
     
     const invoice = reactive({
-      billFromAddress: '',
-      billFromCity: '',
-      billFromZipCode: '',
-      billFromState: '',
-      billToName: '',
-      billToEmail: '',
-      billToAddress: '',
-      billToCity: '',
-      billToZipCode: '',
-      billToState: '',
+      from: {
+        address: '',
+        city: '',
+        zipCode: '',
+        state: '',
+      },
+      to: {
+        name: '',
+        email: '',
+        address: '',
+        city: '',
+        zipcode: '',
+        state: '',
+      },
       items: []
     })
 
-    function save() {
+    function saveInvoice() {
       store.commit('addInvoiceToList', invoice)
       toggleModal()
     }
@@ -124,14 +63,7 @@ export default {
       store.commit('toggleModal')
     }
 
-    function addInvoiceItems(event) {
-      console.log(event)
-    }
-
-    return { invoice, store, close, save, toggleModal, addInvoiceItems }
+    return { invoice, store, saveInvoice, toggleModal }
   }
 }
 </script>
-
-<style>
-</style>
