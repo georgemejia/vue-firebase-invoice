ba338e958e83191053f378bd58b07d7a114b0b43
George Mejia <gmejiadev@gmail.com>
2022-01-29 18:53:11 -0600

    created to and from form components

---
+++ src/components/InvoiceBillFrom.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-indigo-600 text-sm font-bold mb-2">Bill From</div>
+    <label class="w-full block text-slate-300 text-sm mb-5" for="street">Street Address
+      <input 
+        class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="street" type="text" placeholder="Street Address"
+        v-model='from.address'>
+    </label>
+    
+    <div class="flex mb-10">
+      <label class="w-full block text-slate-300 text-sm" for="city">City
+        <input 
+          class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="city" type="text" placeholder="City"
+          v-model='from.city'>
+      </label>
+      <label class="w-full block mx-3 text-slate-300 text-sm" for="code">Zip Code
+        <input 
+          class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="code" type="text" placeholder="Code"
+          v-model='from.zipcode'>
+      </label>
+      <label class="w-full block text-slate-300 text-sm" for="country">State
+        <input 
+          class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="country" type="text" placeholder="State"
+          v-model='from.state'>
+      </label>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['from']
+}
+</script>
---
+++ src/components/InvoiceBillTo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="text-indigo-600 text-sm font-bold mb-2">Bill To</div>
+  <label class="w-full block text-slate-300 text-sm mb-5" for="street">Client Name
+    <input 
+      class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="street" type="text" placeholder="Name"
+      v-model='to.name'>
+  </label>
+  <label class="w-full block text-slate-300 text-sm mb-5" for="street">Client Email
+    <input 
+      class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="street" type="text" placeholder="Email"
+      v-model='to.email'>
+  </label>
+  <label class="w-full block text-slate-300 text-sm mb-5" for="street">Street Address
+    <input 
+      class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="street" type="text" placeholder="Street Address"
+      v-model='to.address'  >
+  </label>
+  <div class="flex mb-10">
+    <label class="w-full block text-slate-300 text-sm" for="city">City
+      <input 
+        class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="city" type="text" placeholder="City"
+        v-model='to.city'
+      >
+    </label>
+    <label class="w-full block mx-3 text-slate-300 text-sm" for="code">Zip Code
+      <input 
+        class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="code" type="text" placeholder="Code"
+        v-model='to.zipcode'>
+    </label>
+    <label class="w-full block text-slate-300 text-sm" for="country">State
+      <input 
+        class="w-full bg-slate-700 block p-2 mt-2 rounded placeholder:text-sm" id="country" type="text" placeholder="State"
+        v-model='to.state'>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['to']
+}
+</script>
